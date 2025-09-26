16eec3ab5356531015a7bd1134277b311b750bab
Adrsb <adrian.sbar07@gmail.com>
2025-09-26 07:47:26 -0300

    Adição da função accounting_summary em analysis.summary

--- src/analysis/summary/accounting.py
+++ src/analysis/summary/accounting.py
@@ -24,7 +24,7 @@ FINAL_COLUMNS = [
 CUSTOM_INDEX = [0, 1, 2, 3, 4, 5, 6, 6.1, 6.2]
 
 
-def budget_seed_summary(accounting_data: pd.DataFrame) -> pd.DataFrame:
+def budget_summary(accounting_data: pd.DataFrame) -> pd.DataFrame:
     """
     Gera um resumo orçamentário a partir dos dados contábeis.
 
@@ -60,3 +60,44 @@ def budget_seed_summary(accounting_data: pd.DataFrame) -> pd.DataFrame:
     budget_summary.columns = FINAL_COLUMNS
 
     return budget_summary
+
+
+# Constantes de colunas
+SUMMARY_VALUES = [
+    "Dotação Atualizada",
+    "Despesas Empenhadas",
+    "Despesas Liquidadas",
+    "Despesas do Exercício Pagas",
+    "Despesas Pagas RAP",
+    "Restos a Pagar do Exercício",
+    "RAP do Exercício Processados",
+    "RAP do Exercício Não Processados",
+]
+
+SUMMARY_INDEX = ["Classificação", "Natureza"]
+
+
+def accounting_summary(accounting_data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Gera uma tabela dinâmica (pivot table) com o resumo das despesas contábeis.
+
+    Args:
+        accounting_data (pd.DataFrame): DataFrame já transformado contendo
+            colunas de valores e classificações.
+
+    Returns:
+        pd.DataFrame: DataFrame resumo com totais agregados por classificação e natureza.
+    """
+    summary = (
+        pd.pivot_table(
+            data=accounting_data,
+            values=SUMMARY_VALUES,
+            index=SUMMARY_INDEX,
+            aggfunc="sum",
+            margins=True,
+            margins_name="TOTAL",
+        )
+        .reset_index()
+        .loc[:, SUMMARY_INDEX + SUMMARY_VALUES]  # garante ordem das colunas
+    )
+    return summary
